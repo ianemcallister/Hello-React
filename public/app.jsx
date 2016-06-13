778e0f1705bd3b0b8573feb1fac0c6e64f22d8d7
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2016-06-12 20:28:03 -0700

    feat: Adding a Textarea for Message
    Passing the message through aswell.

--- public/app.jsx
+++ public/app.jsx
@@ -17,18 +17,28 @@ var GreeterForm = React.createClass({
 	onFormSubmit: function(e) {
 		e.preventDefault();
 
+		var updates = {};
 		var name = this.refs.name.value;
+		var message = this.refs.message.value;
 
 		if (name.length > 0) {
 			this.refs.name.value = '';
-			this.props.onNewName(name);
+			updates.name = name;
 		}
+
+		if (message.length > 0) {
+			this.refs.message.value = '';
+			updates.message = message;
+		}
+
+		this.props.onNewData(updates);
 	},
 	render: function() {
 		return (
 			<form onSubmit={this.onFormSubmit}>
-				<input type='text' ref='name'/>
-				<button>Set Name</button>
+				<input type='text' ref='name' placeholder="Enter Name"/>
+				<textarea ref='message' placeholder="Enter Message"></textarea>
+				<button>Submit</button>
 			</form>			
 		);
 
@@ -45,24 +55,27 @@ var Greeter = React.createClass({
 	},
 	getInitialState: function () {
 		return {
-			name: this.props.name
+			name: this.props.name,
+			name: this.props.message
 		};
 	},
-	handleNewName: function(name) {
-		this.setState({
-			name: name
-		});
+	handleNewData: function(updates) {
+		this.setState(updates);
 	},
 	render: function() {
 		var name = this.state.name;
-		var message = this.props.message;
+		var message = this.state.message;
 
 		return (
 			<div>
 
-				<GreeterMessage name={name} message={message}/>
+				<div>
+					<GreeterMessage name={name} message={message}/>
+				</div>
 
-				<GreeterForm onNewName={this.handleNewName}/>
+				<div>
+					<GreeterForm onNewData={this.handleNewData}/>
+				</div>
 
 			</div>
 		);
